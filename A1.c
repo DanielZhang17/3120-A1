2f25bb4fabe01d11e740276fb3e8c43bcf7739d3
daniel <zhanganran2011@live.com>
2020-05-15 00:08:41 -0300

    Complete all functionality

--- A1.c
+++ A1.c
@@ -9,6 +9,8 @@
  *           of days to simulate.
  * Notes:    rand() does not guarantee randomness so the chances may deviate.
  *           Floating points are rounded to 2 decimals in the output.
+ *           Should not have a upper limit for the number of days unless out of
+ *           memory.
  *           Command used to compile: gcc A1.c -o A1.out -lm
  */
 #include <stdio.h>
@@ -28,6 +30,7 @@ struct Customer
     int haircut_time;
     int wait_time;
 };
+float result[4];
 
 // function declarations
 int getRand(int min, int max);
@@ -35,8 +38,8 @@ struct Customer getCustomer(int time);
 float * simulation();
 int isInt(char arg[]);
 void printResults(int n,float * output);
+float * multiday(int n);
 
-float result[4];
 
 int main(int argc,char* argv[])
 {
@@ -56,8 +59,8 @@ int main(int argc,char* argv[])
         if (argc>=2 && isInt(argv[1])==1)
         {
             printf("%s%s%s", "You choose to simulate ", argv[1], " days.\n");
-            //TODO:run simulation for the specified # of days.
-
+            int n = atoi(argv[2]);
+            multiday(n);
             return 0;
         }
     }
@@ -73,10 +76,7 @@ int main(int argc,char* argv[])
             printf("%s%s%s", "You choose to simulate ", argv[2], " days.\n");
             //run simulation for the specified # of days.
             int n = atoi(argv[2]);
-            for (int i = 0; i < n; i++)
-            {
-                printResults(i,simulation());
-            }
+            multiday(n);
 
             return 0;
         }
@@ -85,7 +85,7 @@ int main(int argc,char* argv[])
     printf("Simulating one day by default.\n\n");
     float * output;
     output = simulation();
-    printResults(0,output);
+    printResults(1,output);
 
     return 0;
 }
@@ -222,7 +222,7 @@ float * simulation()
     result[1] = avg_waiting;
     result[2]= sdv;
     result[3]= not_served;
-    printf("Total number of customers served: %d\n",current+1);
+    //printf("Total number of customers served: %d\n",current+1);
     return result;
 }
 //generates a random customer
@@ -256,4 +256,81 @@ void printResults(int n, float output[4])
            output[0],output[1],output[2]);
     printf("# of those who waited but didn't get served: %.f.\n",
            output[3]);
+}
+//run the simulation for n days and process the results
+float * multiday(int n)
+{
+    /*  min average wait time                               -> tmp[0]
+     *  max average wait time                               -> tmp[1]
+     *  min standard deviation of wait time                 -> tmp[2]
+     *  max standard deviation of wait time                 -> tmp[3]
+     *  avg daily average wait time                         -> tmp[4]
+     *  standard deviation of daily avg                     -> tmp[5]
+     *  avg daily standard deviation                        -> tmp[6]
+     *  standard deviation of daily standard deviation      -> tmp[7]
+     */
+    float * temp[n];//hold the results for simulations
+    float tmp[8]={0,0,0,0,0,0,0,0};//stores processed data, default: 0
+    float * output = &tmp;
+    for (int i = 0; i < n; ++i)
+    {
+        temp[i] = simulation();
+        //populate the results of daily min and max
+        if (tmp[0]==0)
+        {
+            tmp[0] = temp[i][1];
+            tmp[1] = temp[i][1];
+            tmp[2] = temp[i][2];
+            tmp[3] = temp[i][2];
+        }
+        else
+        {
+            if (temp[i][1]<tmp[0])
+                tmp[0] = temp[i][1];
+            if (temp[i][1]>tmp[1])
+                tmp[1] = temp[1][1];
+            if (temp[i][2]<tmp[2])
+                tmp[2] = temp[i][2];
+            if (temp[i][2]>tmp[3])
+                tmp[3] = temp[i][2];
+        }
+        //standard derivation can't be calculated until all simulation is done
+        tmp[4] +=temp[i][1]/(float)n;
+        tmp[6] +=temp[i][2]/(float)n;
+
+        //print results for each day
+        printResults(i+1,temp[i]);
+    }
+    float sdv_avg = 0;
+    float sdv_sdv = 0;
+    float avg_wait = tmp[4];
+    float avg_sdv = tmp[6];
+    //calculate standard deviation of daily avg
+    for (int i = 0; i < n; ++i)
+    {
+        float x = 0;
+        x+=pow(temp[i][1]-avg_wait,2);
+        sdv_avg += x;
+    }
+    sdv_avg=sqrt(sdv_avg / n);
+
+    //calculate standard deviation of daily standard deviation
+    for (int i = 0; i < n; ++i)
+    {
+        float x = 0;
+        x+=pow(temp[i][2]-avg_sdv,2);
+        sdv_sdv += x;
+    }
+    sdv_sdv=sqrt(sdv_sdv / n);
+    tmp[5] = sdv_avg;
+    tmp[7] = sdv_sdv;
+
+    printf("\nSummary:\n");
+    printf("Daily avg waiting time min: %.2f,max: %.2f\n"
+           "Daily standard deviation of wait time min: %.2f,max: %.2f\n"
+           "Average daily waiting time: %.2f,standard deviation: %.2f\n"
+           "Average of daily standard deviations: %.2f\n"
+           "Standard deviation of daily standard deviations: %.2f\n",
+           tmp[0],tmp[1],tmp[2],tmp[3],tmp[4],tmp[5],tmp[6],tmp[7]);
+    return output;
 }
